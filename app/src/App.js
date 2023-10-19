c83998a6515064a7723464588c5471dba05ab047
Mastangithub123 <rajulaakhila123@gmail.com>
2023-10-19 14:00:53 +0530

    Updated the App.js file

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="title">
-          I am an Example App Running in the Cloud1
+          Welocme to Mastan World ..! you have deployed your application succesfully by Circleci
         </div>
       </div>
     );
