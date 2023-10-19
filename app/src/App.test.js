7517414029c4e093b95d8a131435bfed4fac7e97
Mastangithub123 <rajulaakhila123@gmail.com>
2023-10-19 14:11:58 +0530

    Updated the App.test.js file

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -3,7 +3,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to Matan World ..! you have deployed your application successfully by CircleCI/i);
+  const linkElement = screen.getByText("Welcome to Matan World ..! you have deployed your application successfully by CircleCI");
   expect(linkElement).toBeInTheDocument();
 });
+
 
