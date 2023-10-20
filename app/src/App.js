8f291a3d13f16b316df8e1332aa6e3bbf6e5f414
Mastangithub123 <rajulaakhila123@gmail.com>
2023-10-20 11:17:28 +0530

    changed the ui

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="title">
-          I am an Example App Running in the Cloud1
+        Hii Mastan, you have successfully deployed your React app with CircleCI
         </div>
       </div>
     );
