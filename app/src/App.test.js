c17c48e910121889d1dca64fd151dd80eab36d6d
Mastangithub123 <rajulaakhila123@gmail.com>
2023-10-20 11:22:22 +0530

    changed the ui

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,8 +1,17 @@
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/example app/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/example app/i);
+  const linkElement = screen.queryByText(/Hii Mastan/i);
   expect(linkElement).toBeInTheDocument();
 });
